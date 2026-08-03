fc5ac23d8efcf8d2cba789c6105713ccd4424c26
Muhammad Asif Khalid <asifkhalid03@gmail.com>
2026-08-03 21:59:38 +0300

    Release Git Flow v1.0.13

--- lib/update_service.dart
+++ lib/update_service.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
-const appVersion = '1.0.12';
+const appVersion = '1.0.13';
 
 class UpdateDownloadProgress {
   const UpdateDownloadProgress({
